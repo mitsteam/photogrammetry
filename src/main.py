c6f37b2f96b51fc65a55ae89bd1b2de801c1aaf3
karipov <karipov@protonmail.com>
2021-02-03 15:19:06 +0100

    navigation control

--- src/main.py
+++ src/main.py
@@ -2,30 +2,11 @@ import cv2
 from easytello import tello
 import time
 
-def input_stuff(self) -> list:
-    print("Please input the dimensions of your object in cm below.")
 
-    self.c_width = int(input("Width: "))
-    self.c_length = int(input("Length: "))
-    self.c_alt = int(input("Altitude: "))
-
-    print("Done!")
-
-
-def tour(self):
-    """ Tours around the object and take pictures """
-    while True:
-        k = cv2.waitKey(1)
-
-        if k == 87:
-            self.forward(0.1)
-        elif k == 65
-            self.left(0.1)
-        elif k == 
-
-def video(self):
+def main(self):
     # Creating stream capture object
     cap = cv2.VideoCapture('udp://@192.168.10.1:11111')
+
     # Runs while 'stream_state' is True
     while True:
         ret, last_frame = cap.read()
@@ -33,17 +14,26 @@ def video(self):
 
         # Video Stream is closed if escape key is pressed
         k = cv2.waitKey(1) & 0xFF
-        if k == 27:
+
+        # Control loop
+        if k == 87 or k == 38:  # w or up
+            self.forward(0.1)
+        elif k == 65 or k == 37:  # a or left
+            self.left(0.1)
+        elif k == 83 or k == 40:  # s or down
+            self.back(0.1)
+        elif k == 68 or k == 39:  # d or right
+            self.right(0.1)
+        elif k == 27:  # esc
+            self.land()
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
-drone = tello.Tello()
-setattr(drone, 'video', video)
-setattr(drone, 'input_stuff', input_stuff) 
 
-# drone.input_stuff()
+drone = tello.Tello()
+setattr(drone, 'main', main) 
+
 drone.send_command('streamon')
 drone.video()
-

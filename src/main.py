23365acc83f54d5239eb5d95de453453b55250d4
karipov <karipov@protonmail.com>
2021-02-03 13:11:49 +0100

    input and code cleanup

--- src/main.py
+++ src/main.py
@@ -1,26 +1,17 @@
 import cv2
 from easytello import tello
 import time
-import tkinter
 
-class Config:
-    def __init__(self):
-        self.dimesions = None
+def input_stuff() -> list:
+    print("Please input the dimensions of your object in cm below.")
 
-        self.root = tkinter.Tk()
-        tkinter.Button(self.root, text="Save", command=self.save_destroy)
-        self.root.mainloop()
-    
-    def save_destroy(self):
-        self.root.destroy()
+    width = int(input("Width: "))
+    height = int(input("Height: "))
 
+    return [width, height]
+ 
 
-
-
-def input_stuff():
-    t
-
-def video():
+def video(self):
     # Creating stream capture object
     cap = cv2.VideoCapture('udp://@192.168.10.1:11111')
     # Runs while 'stream_state' is True
@@ -36,32 +27,10 @@ def video():
     cap.release()
     cv2.destroyAllWindows()
 
-<<<<<<< HEAD
 drone = tello.Tello()
-flight_configs = Config()
+setattr(drone, 'video', video)
+
+
 drone.send_command('streamon')
-video()
+drone.video()
 
-=======
-
-def main():
-    while True:
-        k = cv2.waitKey(1)
-        if k == 87 or k == 38: # w
-            tello.forward(0.1)
-        elif k == 65 or k == 37: # a
-            tello.left(0.1)
-        elif k == 83 or k == 40: # s
-            tello.back(0.1)
-        elif k == 68 or k == 39: # d
-            tello.right(0.1)
-        elif k == 27: # esc
-            tello.land()
-            break
-        
-if __name__ == "__main__":
-    drone = tello.Tello()
-    # drone.send_command('streamon')
-    # video
-    main()
->>>>>>> 2d7263c8d7035582f6e808fb745930e9b6208386
